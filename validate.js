6b1e6339d08f3a01621dd693a398e38dc05110f3
mds115577 <70107377+mds115577@users.noreply.github.com>
2022-03-26 12:22:40 +0530

    final

--- validate.js
+++ validate.js
@@ -1,18 +1,32 @@
 
 
 
-function ValidateForm(){
+const form  = document.querySelector("#formio");
+
+form.addEventListener('submit', (e) => {
+
+   const value = ValidateForm();
+
+   e.preventDefault()
+
+   if (value) {
+       alert("Thank you for contacting us.")
+   }
+
+});
+
+ function ValidateForm(){
+    
     let x= document.forms["myform"]["yname"].value;
     if(x==""){
         alert("Please Enter your name");
         return false;
     }else{
-        if(x.length < 4 || x.length > 25){
-            alert("Name must be in between 4 & 25 characters");
+        if(x.length < 3 || x.length > 25){
+            alert("Name must be in between 3 & 25 characters");
             return false;
         }
-    }
-    
+   
     
     let y=document.forms["myform"]["ymail"].value;
     if(y==""){
@@ -27,5 +41,7 @@ if (atposition<1 || dotposition<atposition+2 || dotposition+2>=z.length){
   alert("Please enter a valid e-mail address");  
   return false;  
   } 
+  return true;
 }
 
+ }
